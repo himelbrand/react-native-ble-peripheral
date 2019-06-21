e9d713481f16a8070db03aac8036eaaf5a7f9de0
jesse <jesse.katsumata@cureapp.jp>
2019-06-21 20:06:34 +0900

    feat: add type for isAdvertising

--- index.d.ts
+++ index.d.ts
@@ -14,4 +14,5 @@ declare module "react-native-ble-peripheral" {
   function start(): Promise<void>;
   function stop(): void;
   function setName(name: string): void;
+  function isAdvertising(): Promise<boolean>;
 }
